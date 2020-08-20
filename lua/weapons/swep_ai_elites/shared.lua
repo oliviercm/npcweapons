caa2984d91967c271cf3fef8cf2d78a84ee296cd
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-20 04:49:37 -0700

    Add single Elite

--- lua/weapons/swep_ai_elites/shared.lua
+++ lua/weapons/swep_ai_elites/shared.lua
@@ -1,10 +1,10 @@
 DEFINE_BASECLASS("swep_ai_base")
 
 --[[
---ELITE
+--ELITES
 --]]
 
-SWEP.WorldModel					= "models/weapons/w_pist_elite_single.mdl"
+SWEP.WorldModel					= "models/weapons/w_pist_elite.mdl"
 SWEP.HoldType					= "pistol"
 
 SWEP.MuzzleEffect    			= "MuzzleEffect"
@@ -18,14 +18,14 @@ SWEP.Primary.MaxDropoffDistance	= NPC_WEAPONS_MAX_DROPOFF_DISTANCE_PISTOL
 SWEP.Primary.Force				= 5
 SWEP.Primary.Spread				= 0.022
 SWEP.Primary.SpreadMoveMult		= NPC_WEAPONS_SPREAD_MOVE_MULT_LOW
-SWEP.Primary.BurstMinShots		= 1
-SWEP.Primary.BurstMaxShots		= 1
+SWEP.Primary.BurstMinShots		= 2
+SWEP.Primary.BurstMaxShots		= 2
 SWEP.Primary.BurstMinDelay		= 0.15
 SWEP.Primary.BurstMaxDelay		= 0.25
 SWEP.Primary.FireDelay			= 0.15
 SWEP.Primary.NumBullets			= 1
-SWEP.Primary.ClipSize			= 15
-SWEP.Primary.DefaultClip		= 15
+SWEP.Primary.ClipSize			= 30
+SWEP.Primary.DefaultClip		= 30
 SWEP.Primary.AimDelayMin		= NPC_WEAPONS_MIN_AIM_DELAY_LOW
 SWEP.Primary.AimDelayMax		= NPC_WEAPONS_MAX_AIM_DELAY_LOW
 SWEP.Primary.Sound				= "swep_ai_elite_fire"
