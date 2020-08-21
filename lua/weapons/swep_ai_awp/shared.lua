3a1f2c51204a1cab8abeb79f5272caba27ed1e38
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-20 22:03:26 -0700

    Update AWP shoot effects

--- lua/weapons/swep_ai_awp/shared.lua
+++ lua/weapons/swep_ai_awp/shared.lua
@@ -32,33 +32,37 @@ SWEP.Primary.Sound				= "swep_ai_awp_fire"
 SWEP.Primary.BoltSound			= "swep_ai_awp_bolt"
 
 function SWEP:ShootEffects()
-
-	self.Weapon:EmitSound(self.Primary.Sound)
 	
+	self:EmitSound(self.Primary.Sound)
+
 	timer.Simple(0.8, function()
-		if IsValid(self) and IsValid(self.Weapon) and IsValid(self.Owner) then
-			sound.Play(self.Primary.BoltSound, self.Weapon:GetPos(), 75)
+
+		if IsValid(self) and IsValid(self:GetOwner()) then
+
+			sound.Play(self.Primary.BoltSound, self.Weapon:GetPos(), SNDLVL_NORM)
 			
 			timer.Simple(0.333, function()
-				if IsValid(self) and IsValid(self.Weapon) and IsValid(self.Owner) then
-					sound.Play(self.Primary.BoltSound, self.Weapon:GetPos())
+
+				if IsValid(self) and IsValid(self:GetOwner()) then
+
+					sound.Play(self.Primary.BoltSound, self.Weapon:GetPos(), SNDLVL_NORM)
+
 				end
+				
 			end)
 			
 			local shellEffect = EffectData()
-			shellEffect:SetEntity(self.Weapon)
-			shellEffect:SetOrigin(self.Weapon:GetAttachment(self.ShellAttachment).Pos)
-			shellEffect:SetAngles(self.Weapon:GetAttachment(self.ShellAttachment).Ang)
+			local shellAttach = self:GetAttachment(self.ShellAttachment)
+			shellEffect:SetEntity(self)
+			shellEffect:SetOrigin(shellAttach.Pos)
+			shellEffect:SetAngles(shellAttach.Ang)
+			shellEffect:SetScale(1)
+			shellEffect:SetMagnitude(1)
+			shellEffect:SetRadius(1)
 			util.Effect(self.ShellEffect, shellEffect)
+
 		end
+
 	end)
-	
-	local muzzleEffect = EffectData()
-	muzzleEffect:SetEntity(self.Weapon)
-	muzzleEffect:SetOrigin(self.Weapon:GetAttachment(self.MuzzleAttachment).Pos)
-	muzzleEffect:SetAngles(self.Weapon:GetAttachment(self.MuzzleAttachment).Ang)
-	util.Effect(self.MuzzleEffect, muzzleEffect)
-	
-	self.Owner:MuzzleFlash()
 
 end
