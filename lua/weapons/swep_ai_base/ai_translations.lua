04b9acedb822192c0204ac9f8dfc2802fdedbcd2
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-21 16:25:49 -0700

    Update Pistol holdtype

--- lua/weapons/swep_ai_base/ai_translations.lua
+++ lua/weapons/swep_ai_base/ai_translations.lua
@@ -1,5 +1,8 @@
 function SWEP:SetupWeaponHoldTypeForAI(t)
 
+	local owner = self:GetOwner()
+	local cl = owner:GetClass()
+
 	self.ActivityTranslateAI = {}
 	
 	if t == "ar2" then
@@ -109,29 +112,72 @@ function SWEP:SetupWeaponHoldTypeForAI(t)
 		return
 		
 	elseif t == "pistol" then
-	
-		self.ActivityTranslateAI [ACT_IDLE]									= ACT_IDLE_ANGRY_SMG1
-		self.ActivityTranslateAI [ACT_IDLE_RELAXED] 						= ACT_IDLE_ANGRY_SMG1
-		self.ActivityTranslateAI [ACT_IDLE_STIMULATED] 						= ACT_IDLE_ANGRY_SMG1
-		self.ActivityTranslateAI [ACT_IDLE_AGITATED] 						= ACT_IDLE_ANGRY_SMG1
-		self.ActivityTranslateAI [ACT_IDLE_ANGRY]							= ACT_IDLE_ANGRY_SMG1
-		
-		self.ActivityTranslateAI [ACT_IDLE_AIM_RELAXED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
-		self.ActivityTranslateAI [ACT_IDLE_AIM_STIMULATED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
-		self.ActivityTranslateAI [ACT_IDLE_AIM_AGITATED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
-		
-		self.ActivityTranslateAI [ACT_WALK] 								= ACT_WALK_AIM
-		self.ActivityTranslateAI [ACT_WALK_RELAXED] 						= ACT_WALK_AIM_RELAXED
-		self.ActivityTranslateAI [ACT_WALK_STIMULATED] 						= ACT_WALK_AIM_STIMULATED
-		self.ActivityTranslateAI [ACT_WALK_AGITATED] 						= ACT_WALK_AIM_AGITATED
-		
-		self.ActivityTranslateAI [ACT_RUN] 									= ACT_RUN_AIM
-		self.ActivityTranslateAI [ACT_RUN_RELAXED] 							= ACT_RUN_AIM_RELAXED
-		self.ActivityTranslateAI [ACT_RUN_STIMULATED] 						= ACT_RUN_AIM_STIMULATED
-		self.ActivityTranslateAI [ACT_RUN_AGITATED] 						= ACT_RUN_AIM_AGITATED
 
-		if self:GetOwner():GetClass() == "npc_citizen" then
-		
+		if cl == "npc_metropolice" or cl == "npc_alyx" or (cl == "npc_citizen" and string.find(owner:GetModel(), "female")) then
+
+			self.ActivityTranslateAI [ACT_IDLE]									= ACT_IDLE_ANGRY_PISTOL
+			self.ActivityTranslateAI [ACT_IDLE_RELAXED] 						= ACT_IDLE_ANGRY_PISTOL
+			self.ActivityTranslateAI [ACT_IDLE_STIMULATED] 						= ACT_IDLE_ANGRY_PISTOL
+			self.ActivityTranslateAI [ACT_IDLE_AGITATED] 						= ACT_IDLE_ANGRY_PISTOL
+			self.ActivityTranslateAI [ACT_IDLE_ANGRY]							= ACT_IDLE_ANGRY_PISTOL
+
+			self.ActivityTranslateAI [ACT_IDLE_AIM_RELAXED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
+			self.ActivityTranslateAI [ACT_IDLE_AIM_STIMULATED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
+			self.ActivityTranslateAI [ACT_IDLE_AIM_AGITATED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
+			
+			self.ActivityTranslateAI [ACT_WALK] 								= ACT_WALK_AIM
+			self.ActivityTranslateAI [ACT_WALK_RELAXED] 						= ACT_WALK_AIM_RELAXED
+			self.ActivityTranslateAI [ACT_WALK_STIMULATED] 						= ACT_WALK_AIM_STIMULATED
+			self.ActivityTranslateAI [ACT_WALK_AGITATED] 						= ACT_WALK_AIM_AGITATED
+			
+			self.ActivityTranslateAI [ACT_RUN] 									= ACT_RUN_AIM
+			self.ActivityTranslateAI [ACT_RUN_RELAXED] 							= ACT_RUN_AIM_RELAXED
+			self.ActivityTranslateAI [ACT_RUN_STIMULATED] 						= ACT_RUN_AIM_STIMULATED
+			self.ActivityTranslateAI [ACT_RUN_AGITATED] 						= ACT_RUN_AIM_AGITATED
+
+			self.ActivityTranslateAI [ACT_WALK_AIM] 							= ACT_WALK_AIM_PISTOL
+			self.ActivityTranslateAI [ACT_WALK_AIM_RELAXED] 					= ACT_WALK_AIM_PISTOL
+			self.ActivityTranslateAI [ACT_WALK_AIM_STIMULATED] 					= ACT_WALK_AIM_PISTOL
+			self.ActivityTranslateAI [ACT_WALK_AIM_AGITATED] 					= ACT_WALK_AIM_PISTOL
+			
+			self.ActivityTranslateAI [ACT_RUN_AIM]								= ACT_RUN_AIM_PISTOL
+			self.ActivityTranslateAI [ACT_RUN_AIM_RELAXED] 						= ACT_RUN_AIM_PISTOL
+			self.ActivityTranslateAI [ACT_RUN_AIM_STIMULATED] 					= ACT_RUN_AIM_PISTOL
+			self.ActivityTranslateAI [ACT_RUN_AIM_AGITATED] 					= ACT_RUN_AIM_PISTOL
+
+			self.ActivityTranslateAI [ACT_RANGE_ATTACK1]						= ACT_RANGE_ATTACK_PISTOL
+			self.ActivityTranslateAI [ACT_RELOAD]								= ACT_RELOAD_PISTOL
+			
+			self.ActivityTranslateAI [ACT_GESTURE_RANGE_ATTACK1]				= ACT_GESTURE_RANGE_ATTACK_PISTOL
+			self.ActivityTranslateAI [ACT_GESTURE_RELOAD]						= ACT_GESTURE_RELOAD_PISTOL
+			
+			self.ActivityTranslateAI [ACT_RANGE_ATTACK1_LOW]					= ACT_RANGE_ATTACK_PISTOL_LOW
+			self.ActivityTranslateAI [ACT_RANGE_AIM_LOW]						= ACT_RANGE_AIM_PISTOL_LOW
+			self.ActivityTranslateAI [ACT_RELOAD_LOW]							= ACT_RELOAD_PISTOL_LOW
+			self.ActivityTranslateAI [ACT_COVER_LOW]							= ACT_COVER_PISTOL_LOW
+
+		else
+
+			self.ActivityTranslateAI [ACT_IDLE]									= ACT_IDLE_ANGRY_SMG1
+			self.ActivityTranslateAI [ACT_IDLE_RELAXED] 						= ACT_IDLE_ANGRY_SMG1
+			self.ActivityTranslateAI [ACT_IDLE_STIMULATED] 						= ACT_IDLE_ANGRY_SMG1
+			self.ActivityTranslateAI [ACT_IDLE_AGITATED] 						= ACT_IDLE_ANGRY_SMG1
+			self.ActivityTranslateAI [ACT_IDLE_ANGRY]							= ACT_IDLE_ANGRY_SMG1
+
+			self.ActivityTranslateAI [ACT_IDLE_AIM_RELAXED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
+			self.ActivityTranslateAI [ACT_IDLE_AIM_STIMULATED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
+			self.ActivityTranslateAI [ACT_IDLE_AIM_AGITATED] 					= ACT_IDLE_AIM_RIFLE_STIMULATED
+			
+			self.ActivityTranslateAI [ACT_WALK] 								= ACT_WALK_AIM
+			self.ActivityTranslateAI [ACT_WALK_RELAXED] 						= ACT_WALK_AIM_RELAXED
+			self.ActivityTranslateAI [ACT_WALK_STIMULATED] 						= ACT_WALK_AIM_STIMULATED
+			self.ActivityTranslateAI [ACT_WALK_AGITATED] 						= ACT_WALK_AIM_AGITATED
+			
+			self.ActivityTranslateAI [ACT_RUN] 									= ACT_RUN_AIM
+			self.ActivityTranslateAI [ACT_RUN_RELAXED] 							= ACT_RUN_AIM_RELAXED
+			self.ActivityTranslateAI [ACT_RUN_STIMULATED] 						= ACT_RUN_AIM_STIMULATED
+			self.ActivityTranslateAI [ACT_RUN_AGITATED] 						= ACT_RUN_AIM_AGITATED
+
 			self.ActivityTranslateAI [ACT_WALK_AIM] 							= ACT_WALK_AIM_RIFLE_STIMULATED
 			self.ActivityTranslateAI [ACT_WALK_AIM_RELAXED] 					= ACT_WALK_AIM_RIFLE_STIMULATED
 			self.ActivityTranslateAI [ACT_WALK_AIM_STIMULATED] 					= ACT_WALK_AIM_RIFLE_STIMULATED
@@ -141,31 +187,19 @@ function SWEP:SetupWeaponHoldTypeForAI(t)
 			self.ActivityTranslateAI [ACT_RUN_AIM_RELAXED] 						= ACT_RUN_AIM_RIFLE_STIMULATED
 			self.ActivityTranslateAI [ACT_RUN_AIM_STIMULATED] 					= ACT_RUN_AIM_RIFLE_STIMULATED
 			self.ActivityTranslateAI [ACT_RUN_AIM_AGITATED] 					= ACT_RUN_AIM_RIFLE_STIMULATED
+
+			self.ActivityTranslateAI [ACT_RANGE_ATTACK1]						= ACT_RANGE_ATTACK_PISTOL
+			self.ActivityTranslateAI [ACT_RELOAD]								= ACT_RELOAD_PISTOL
 			
-		else
-		
-			self.ActivityTranslateAI [ACT_WALK_AIM] 							= ACT_WALK_AIM_RIFLE
-			self.ActivityTranslateAI [ACT_WALK_AIM_RELAXED] 					= ACT_WALK_AIM_RIFLE
-			self.ActivityTranslateAI [ACT_WALK_AIM_STIMULATED] 					= ACT_WALK_AIM_RIFLE
-			self.ActivityTranslateAI [ACT_WALK_AIM_AGITATED] 					= ACT_WALK_AIM_RIFLE
-		
-			self.ActivityTranslateAI [ACT_RUN_AIM]								= ACT_RUN_AIM_RIFLE
-			self.ActivityTranslateAI [ACT_RUN_AIM_RELAXED] 						= ACT_RUN_AIM_RIFLE
-			self.ActivityTranslateAI [ACT_RUN_AIM_STIMULATED] 					= ACT_RUN_AIM_RIFLE
-			self.ActivityTranslateAI [ACT_RUN_AIM_AGITATED] 					= ACT_RUN_AIM_RIFLE
-		
+			self.ActivityTranslateAI [ACT_GESTURE_RANGE_ATTACK1]				= ACT_GESTURE_RANGE_ATTACK_PISTOL
+			self.ActivityTranslateAI [ACT_GESTURE_RELOAD]						= ACT_GESTURE_RELOAD_PISTOL
+			
+			self.ActivityTranslateAI [ACT_RANGE_ATTACK1_LOW]					= ACT_RANGE_ATTACK_PISTOL_LOW
+			self.ActivityTranslateAI [ACT_RANGE_AIM_LOW]						= ACT_RANGE_AIM_PISTOL_LOW
+			self.ActivityTranslateAI [ACT_RELOAD_LOW]							= ACT_RELOAD_PISTOL_LOW
+			self.ActivityTranslateAI [ACT_COVER_LOW]							= ACT_COVER_PISTOL_LOW
+
 		end
-		
-		self.ActivityTranslateAI [ACT_RANGE_ATTACK1]						= ACT_RANGE_ATTACK_PISTOL
-		self.ActivityTranslateAI [ACT_RELOAD]								= ACT_RELOAD_PISTOL
-		
-		self.ActivityTranslateAI [ACT_GESTURE_RANGE_ATTACK1]				= ACT_GESTURE_RANGE_ATTACK_PISTOL
-		self.ActivityTranslateAI [ACT_GESTURE_RELOAD]						= ACT_GESTURE_RELOAD_PISTOL
-		
-		self.ActivityTranslateAI [ACT_RANGE_ATTACK1_LOW]					= ACT_RANGE_ATTACK_PISTOL_LOW
-		self.ActivityTranslateAI [ACT_RANGE_AIM_LOW]						= ACT_RANGE_AIM_PISTOL_LOW
-		self.ActivityTranslateAI [ACT_RELOAD_LOW]							= ACT_RELOAD_PISTOL_LOW
-		self.ActivityTranslateAI [ACT_COVER_LOW]							= ACT_COVER_PISTOL_LOW
 		
 		return
 
