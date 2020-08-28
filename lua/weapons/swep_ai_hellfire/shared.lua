1dfd2e395698a35ff8ff8d24e083170bc834c531
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-28 05:50:52 -0700

    Update projectiles

--- lua/weapons/swep_ai_hellfire/shared.lua
+++ lua/weapons/swep_ai_hellfire/shared.lua
@@ -15,7 +15,8 @@ SWEP.ReloadSounds				= {{2.0, "swep_ai_automaticpistol_cock"}}
 
 SWEP.ReloadTime					= NPC_WEAPONS_RELOAD_TIME_HIGH
 
-SWEP.Primary.Type               = "rocket"
+SWEP.Primary.Type               = "projectile"
+SWEP.Primary.DamageType         = DMG_BLAST
 SWEP.Primary.DamageMin			= 40
 SWEP.Primary.DamageMax			= 40
 SWEP.Primary.Force				= 0
@@ -33,11 +34,24 @@ SWEP.Primary.AimDelayMin		= NPC_WEAPONS_MIN_AIM_DELAY_HIGH
 SWEP.Primary.AimDelayMax		= NPC_WEAPONS_MAX_AIM_DELAY_HIGH
 SWEP.Primary.Sound				= "swep_ai_rocketlauncher_fire"
 
-SWEP.RocketModel                = "models/weapons/w_missile.mdl"
-SWEP.RocketExplosionRadius		= 200
-SWEP.RocketStartSpeed			= 1000
-SWEP.RocketAcceleration			= 0
-SWEP.RocketRocketRotationSpeed  = 1000
-SWEP.RocketLoopingSound         = "ai_rocket_projectile_sound"
+SWEP.AimForBody     			= true
 
-SWEP.AimForHeadTable			= {}
+SWEP.ProjectileModel            = "models/weapons/w_missile.mdl"
+SWEP.ProjectileStartSpeed       = 1000
+SWEP.ProjectileAcceleration	    = 0
+SWEP.ProjectileHitEffect        = { Name = "Explosion", Radius = 1, Magnitude = 1, Scale = 1 }
+SWEP.ProjectileHitSound         = nil
+SWEP.ProjectileLoopingSound     = "ai_rocket_projectile_sound"
+SWEP.ProjectileRotationSpeed    = 1000
+SWEP.ProjectileIsExplosive      = true
+SWEP.ProjectileExplosionRadius  = 200
+SWEP.ProjectileTrail            = {
+    Attachment = 0,
+    Color = Color(255, 255, 255, 200),
+    Additive = true,
+    StartWidth = 5,
+    EndWidth = 0,
+    Lifetime = 0.3,
+    TextureRes = 0,
+    Texture = "trails/smoke.vmt",
+}
--- lua/weapons/swep_ai_mrl/shared.lua
+++ lua/weapons/swep_ai_mrl/shared.lua
@@ -15,7 +15,8 @@ SWEP.ReloadSounds				= {{2.0, "swep_ai_automaticpistol_cock"}}
 
 SWEP.ReloadTime					= NPC_WEAPONS_RELOAD_TIME_HIGH
 
-SWEP.Primary.Type               = "rocket"
+SWEP.Primary.Type               = "projectile"
+SWEP.Primary.DamageType         = DMG_BLAST
 SWEP.Primary.DamageMin			= 50
 SWEP.Primary.DamageMax			= 50
 SWEP.Primary.Force				= 0
@@ -33,11 +34,24 @@ SWEP.Primary.AimDelayMin		= NPC_WEAPONS_MIN_AIM_DELAY_HIGH
 SWEP.Primary.AimDelayMax		= NPC_WEAPONS_MAX_AIM_DELAY_HIGH
 SWEP.Primary.Sound				= "swep_ai_rocketlauncher_fire"
 
-SWEP.RocketModel                = "models/weapons/w_missile.mdl"
-SWEP.RocketExplosionRadius		= 250
-SWEP.RocketStartSpeed			= 500
-SWEP.RocketAcceleration			= 200
-SWEP.RocketRocketRotationSpeed  = 1000
-SWEP.RocketLoopingSound         = "ai_rocket_projectile_sound"
+SWEP.AimForBody     			= true
 
-SWEP.AimForHeadTable			= {}
+SWEP.ProjectileModel            = "models/weapons/w_missile.mdl"
+SWEP.ProjectileStartSpeed       = 500
+SWEP.ProjectileAcceleration	    = 200
+SWEP.ProjectileHitEffect        = { Name = "Explosion", Radius = 1, Magnitude = 1, Scale = 1 }
+SWEP.ProjectileHitSound         = nil
+SWEP.ProjectileLoopingSound     = "ai_rocket_projectile_sound"
+SWEP.ProjectileRotationSpeed    = 1000
+SWEP.ProjectileIsExplosive      = true
+SWEP.ProjectileExplosionRadius  = 250
+SWEP.ProjectileTrail            = {
+    Attachment = 0,
+    Color = Color(255, 255, 255, 200),
+    Additive = true,
+    StartWidth = 5,
+    EndWidth = 0,
+    Lifetime = 0.3,
+    TextureRes = 0,
+    Texture = "trails/smoke.vmt",
+}
